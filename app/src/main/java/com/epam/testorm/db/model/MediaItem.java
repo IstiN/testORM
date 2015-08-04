2e623d33ecd0005f09f3082c0487b74a6385a97b
Mike <agidel2012@gmail.com>
2015-08-05 00:14:03 +0300

    lost files

---
+++ app/src/main/java/com/epam/testorm/db/model/MediaItem.java
@@ -0,0 +1,33 @@
+package com.epam.testorm.db.model;
+
+import com.epam.testorm.db.ICache;
+import com.epam.testorm.db.annotation.dbString;
+
+/**
+ * Created by Mikhail_Ivanou on 8/4/2015.
+ */
+public class MediaItem implements ICache {
+
+    @dbString
+    public static String URL = "url";
+
+    @dbString
+    public static String TITLE = "title";
+
+    @dbString
+    public static String IMAGE = "image";
+
+    @dbString
+    public static String ORIGINAL = "original";
+
+    @dbString
+    public static String THUMBNAIL = "thumbnail";
+
+    @dbString
+    public static String DESCRIPTION = "description";
+
+    @Override
+    public String getTableName() {
+        return "mediaItem";
+    }
+}
