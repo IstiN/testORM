ee066c2b28c39f979382a88e90afff969cbc4bbe
IstiN (Uladzimir Klyshevich) <istin2007@gmail.com>
2015-08-06 12:36:02 +0300

    xcore fixed

--- app/src/main/java/com/epam/testorm/xcore/XCoreManager.java
+++ app/src/main/java/com/epam/testorm/xcore/XCoreManager.java
@@ -3,50 +3,32 @@ package com.epam.testorm.xcore;
 import android.app.Activity;
 import android.widget.ListAdapter;
 
+import com.epam.testorm.BuildConfig;
 import com.epam.testorm.ICacheManager;
-import com.epam.testorm.xcore.model.XAuthor;
-import com.epam.testorm.xcore.model.XMediaItem;
-import com.epam.testorm.xcore.model.XNews;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 import by.istin.android.xcore.XCoreHelper;
-import by.istin.android.xcore.provider.IDBContentProviderSupport;
+import by.istin.android.xcore.utils.StringUtil;
 
 /**
  * Created by Mikhail_Ivanou on 7/29/2015.
  */
-public class XCoreManager implements ICacheManager{
-
-    private static final Class<?>[] ENTITIES = new Class<?>[]{
-            XNews.class,
-            XMediaItem.class,
-//            XContent.class,
-            XAuthor.class,
-//            XMediaImages.class,
-//            XMediaAudios.class,
-//            XMediaLinks.class,
-//            XMediaVideos.class
-    };
-
-    private Activity mActivity;
-    private StreamProcessor processor;
+public class XCoreManager implements ICacheManager {
 
     public XCoreManager(Activity activity) {
-        this.mActivity = activity;
         XCoreHelper xCoreHelper = XCoreHelper.get();
-        xCoreHelper.onCreate(activity, new ArrayList<Class<? extends XCoreHelper.Module>>(), null);
-        IDBContentProviderSupport dbContentProviderSupport = xCoreHelper.registerContentProvider(ENTITIES);
-        processor = new StreamProcessor(dbContentProviderSupport);
+        xCoreHelper.onCreate(activity.getApplicationContext(), Arrays.<Class<? extends XCoreHelper.Module>>asList(Module.class), BuildConfig.class);
     }
 
     @Override
     public void processFeed(String string) {
         try {
-            InputStream stream = new ByteArrayInputStream(string.getBytes("UTF_8"));
-            processor.execute(null, null, stream);
+            StreamProcessor streamProcessor = (StreamProcessor) XCoreHelper.get().getSystemService(StreamProcessor.APP_SERVICE_KEY);
+            InputStream stream = new ByteArrayInputStream(string.getBytes(StringUtil.DEFAULT_ENCODING));
+            streamProcessor.execute(null, null, stream);
         } catch (Exception e) {
             e.printStackTrace();
         }
